e6dd1e001bebf943ffd100edbd8280bd21038654
bin386@163.com <bin386@163.com>
2020-04-22 23:16:40 +0800

    pages/permission/index.js代码重构，模态全部抽成独立class子组件，通创建角色，跟新增设置权限模态全部通过props父组件像子组件传值渲染，Fix点击弹出模态数据无法反显bug，通过给子组件设置ref,模态增加forceRender，父组件通过refs引用子组件实例调用ref的setFieldsValue实现

--- src/pages/permission/index.js
+++ src/pages/permission/index.js
@@ -29,8 +29,6 @@ const layout = {
 
 export default class PermissionUser extends React.Component{
 
-    formRef = React.createRef();
-
     params = {
         page:1
     }
@@ -38,10 +36,8 @@ export default class PermissionUser extends React.Component{
     state = {
         list:[],
         isRoleVisible:false,
-        userInfo:{
-            roleName:'',
-            state:1,
-        },
+        isPermVisible:false,
+        detailInfo:{}
     }
 
     UNSAFE_componentWillMount(){
@@ -61,10 +57,10 @@ export default class PermissionUser extends React.Component{
 
     // 创建角色提交
     handleRoleSubmit = () => {
-        let data = this.formRef.current.getFieldValue();
+        let data = this.refs.createRole.createRoleFormRef.current.getFieldValue();
         if(!data.roleName){
             message.warning('请输入角色名称');
-        }else if(!data.state){
+        }else if(!data.status){
             message.warning('请选择状态');
         }else{
             axios.ajax({
@@ -81,12 +77,40 @@ export default class PermissionUser extends React.Component{
                         isRoleVisible:false
                     });
                     this.requestList();
-                    this.formRef.current.resetFields();
+                    this.refs.createRole.createRoleFormRef.current.resetFields();
                 }else{
                     message.error('创建角色失败');
                 }
             });
         }
+    }
+
+    // 设置权限
+    handlePermission = () => {
+        let item = this.state.selectedItem;
+        if(!item){
+            Modal.info({
+                title:'提示',
+                content:'请选择一个角色'
+            });
+            return;
+        }
+        this.setState({
+            isPermVisible:true,
+            detailInfo:item
+        });
+        // initialValues 不能被 setState 动态更新，需要用 setFieldsValue 来更新
+        // 修改子组件模态表单值
+        this.refs.permEdit.permEditFormRef.current.setFieldsValue({
+            roleName:item.role_name,
+            status:item.status,
+        });
+    }
+
+    handlePermEditSubmit = () => {
+        this.setState({
+            isPermVisible:false
+        });
     }
 
     render(){
@@ -107,7 +131,7 @@ export default class PermissionUser extends React.Component{
                 title:'使用状态',
                 dataIndex:'status',
                 render(status){
-                    return status === 1?'启用':'停用'
+                    return status === 1?'开启':'关闭'
                 }
             },
             {
@@ -124,7 +148,7 @@ export default class PermissionUser extends React.Component{
             <div style={{width:'100%'}}>
                 <Card>
                     <Button type="primary" icon={<PlusOutlined />} style={{marginRight:'10px'}} onClick={()=>this.handleRole()}>创建角色</Button>
-                    <Button type="primary" style={{marginRight:'10px'}}>设置权限</Button>
+                    <Button type="primary" style={{marginRight:'10px'}} onClick={this.handlePermission}>设置权限</Button>
                     <Button type="primary" >用户授权</Button>
                 </Card>
                 <div className="content-wrap">
@@ -143,41 +167,122 @@ export default class PermissionUser extends React.Component{
                     visible={this.state.isRoleVisible}
                     onOk={this.handleRoleSubmit}
                     onCancel={()=>{
-                        this.formRef.current.resetFields();
+                        this.refs.createRole.createRoleFormRef.current.resetFields(); //这是一个屎坑
                         this.setState({
                             isRoleVisible:false
                         })
                     }}
                     width={600}
                 >
-                    <Form 
-                        {...layout} 
-                        layout="horizontal"
-                        ref={this.formRef} 
-                        name="control-ref" 
-                        onFinish={this.onFinish} 
-                        initialValues={{
-                            roleName:this.state.userInfo.roleName,
-                            state:this.state.userInfo.state,
-                        }}
-                    >
-                        <Form.Item 
-                            name="roleName"
-                            label="角色名称"
-                            rules={[{ required: true }]} 
-                        >
-                            <Input type="text" placeholder="请输入角色名称" />
-                        </Form.Item>
-                        <Form.Item name="state" label="状态" rules={[{ required: true }]}>
-                            <Select
-                                allowClear
-                            >
-                                <Option value={1}>开启</Option>
-                                <Option value={0}>关闭</Option>
-                            </Select>
-                        </Form.Item>
-                    </Form>
+                    <CreateRoleForm 
+                        ref='createRole' 
+                    />
                 </Modal>
+                <Modal
+                    // 屎坑
+                    // 在调用 form 方法时，Modal 还未初始化导致 form 没有关联任何 Form 组件。
+                    // 你可以通过给 Modal 设置 forceRender 将其预渲染。
+                    forceRender 
+                    title='设置权限'
+                    visible={this.state.isPermVisible}
+                    onOk={this.handlePermEditSubmit}
+                    onCancel={()=>{
+                        this.setState({
+                            isPermVisible:false
+                        });
+                    }}
+                    width={600}
+                >
+                    <PermEditForm 
+                        ref='permEdit' 
+                        detailInfo={this.state.detailInfo}
+                    />
+                </Modal>
+            </div>
+        );
+    }
+}
+
+class CreateRoleForm extends React.Component{
+
+    createRoleFormRef = React.createRef();
+
+    state = {
+        userInfo:{
+            roleName:'',
+            status:1,
+        }
+    }
+
+    render(){
+        return(
+            <Form 
+                {...layout} 
+                layout="horizontal"
+                ref={this.createRoleFormRef} 
+                name="control-ref" 
+                onFinish={this.onFinish} 
+                initialValues={{
+                    roleName:this.state.userInfo.roleName,
+                    status:this.state.userInfo.status,
+                }}
+            >
+                <Form.Item 
+                    name="roleName"
+                    label="角色名称"
+                    rules={[{ required: true }]} 
+                >
+                    <Input type="text" placeholder="请输入角色名称" />
+                </Form.Item>
+                <Form.Item name="status" label="状态" rules={[{ required: true }]}>
+                    <Select
+                        allowClear
+                    >
+                        <Option value={1}>开启</Option>
+                        <Option value={0}>关闭</Option>
+                    </Select>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+
+class PermEditForm extends React.Component{
+
+    permEditFormRef = React.createRef();
+
+    state = {}
+
+    render(){
+        return(
+            <div>
+                <Form 
+                    {...layout} 
+                    layout="horizontal"
+                    ref={this.permEditFormRef} 
+                    name="control-ref" 
+                    onFinish={this.onFinish} 
+                    initialValues={{
+                        roleName:this.props.detailInfo.role_name,
+                        status:this.props.detailInfo.status,
+                    }}
+                >
+                    <Form.Item 
+                        name="roleName"
+                        label="角色名称"
+                        rules={[{ required: true }]} 
+                    >
+                        <Input type="text" disabled  />
+                    </Form.Item>
+                    <Form.Item name="status" label="状态" rules={[{ required: true }]}>
+                        <Select
+                            allowClear
+                        >
+                            <Option value={1}>开启</Option>
+                            <Option value={0}>关闭</Option>
+                        </Select>
+                    </Form.Item>
+                </Form>
             </div>
         );
     }
