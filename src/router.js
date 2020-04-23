d3b8f8521ef9a7dd37b295031d11b241c4475e68
bin386@163.com <bin386@163.com>
2020-04-23 14:36:49 +0800

    config/menuConfig去除/admin路径，router.js路由嵌套调整，去除/admin，默认重定向到首页

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import App from './App';
 import Admin from './admin';
 import Buttons from './pages/ui/buttons';
@@ -33,43 +33,46 @@ export default class IRouter extends React.Component{
         return (
             <HashRouter>
                 <App>
-                    {/*登录/主页面/订单详情多入口*/}
-                    <Route path="/login" component={FormLogin} />
-                    <Route path="/admin" render={() => 
-                        <Admin>
-                            <Switch>
-                                <Route path="/admin/home" component={Home} />
-                                <Route path="/admin/ui/buttons" component={Buttons} />
-                                <Route path="/admin/ui/modals" component={Modals} />
-                                <Route path="/admin/ui/loadings" component={Loadings} />
-                                <Route path="/admin/ui/notification" component={Notification} />
-                                <Route path="/admin/ui/messages" component={Messages} />
-                                <Route path="/admin/ui/tabs" component={Tabs} />
-                                <Route path="/admin/ui/gallery" component={Gallery} />
-                                <Route path="/admin/ui/carousel" component={Carousel} />
-                                <Route path="/admin/form/login" component={FormLogin} />
-                                <Route path="/admin/form/register" component={Register} />
-                                <Route path="/admin/table/basic" component={BasicTable} />
-                                <Route path="/admin/table/high" component={HighTable} />
-                                <Route path="/admin/city" component={City} />
-                                <Route path="/admin/order" component={Order} />
-                                <Route path="/admin/user" component={User} />
-                                <Route path="/admin/charts/bar" component={Bar} />
-                                <Route path="/admin/charts/pie" component={Pie} />
-                                <Route path="/admin/charts/line" component={Line} />
-                                <Route path="/admin/rich" component={RichText} />
-                                <Route path="/admin/permission" component={Permission} />
-                                <Route component={NoMatch} />
-                            </Switch>
-                        </Admin>
-                    } />
-                    <Route path="/common" render={()=>
-                        <Common>
-                            {/* 嵌套各种详情 */}
-                            <Route pat="/common/order/detail/:orderId" component={OrderDetail} />
-                        </Common>
-                    }
-                    />
+                    <Switch>
+                        {/*登录/主页面/订单详情多入口*/}
+                        <Route path="/login" component={FormLogin} />
+                        <Route path="/common" render={()=>
+                            <Common>
+                                {/* 嵌套各种详情 */}
+                                <Route pat="/common/order/detail/:orderId" component={OrderDetail} />
+                            </Common>
+                            }
+                        />
+                        <Route path="/" render={() => 
+                            <Admin>
+                                <Switch>
+                                    <Route path="/home" component={Home} />
+                                    <Route path="/ui/buttons" component={Buttons} />
+                                    <Route path="/ui/modals" component={Modals} />
+                                    <Route path="/ui/loadings" component={Loadings} />
+                                    <Route path="/ui/notification" component={Notification} />
+                                    <Route path="/ui/messages" component={Messages} />
+                                    <Route path="/ui/tabs" component={Tabs} />
+                                    <Route path="/ui/gallery" component={Gallery} />
+                                    <Route path="/ui/carousel" component={Carousel} />
+                                    <Route path="/form/login" component={FormLogin} />
+                                    <Route path="/form/register" component={Register} />
+                                    <Route path="/table/basic" component={BasicTable} />
+                                    <Route path="/table/high" component={HighTable} />
+                                    <Route path="/city" component={City} />
+                                    <Route path="/order" component={Order} />
+                                    <Route path="/user" component={User} />
+                                    <Route path="/charts/bar" component={Bar} />
+                                    <Route path="/charts/pie" component={Pie} />
+                                    <Route path="/charts/line" component={Line} />
+                                    <Route path="/rich" component={RichText} />
+                                    <Route path="/permission" component={Permission} />
+                                    <Redirect to="/home" />
+                                    <Route component={NoMatch} />
+                                </Switch>
+                            </Admin>
+                        } />
+                    </Switch>    
                 </App>
             </HashRouter>
         );
